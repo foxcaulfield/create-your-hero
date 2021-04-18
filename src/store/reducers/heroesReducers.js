db4d0d7b82d39b0266edc39cdb1ec831d5830a2e
Fox Caulfield <mrcoderprog@gmail.com>
2021-04-18 17:12:21 +0300

    Add initial state

--- src/store/reducers/heroesReducers.js
+++ src/store/reducers/heroesReducers.js
@@ -1,13 +1,85 @@
+// const KICK_ACTION_CREATOR = "KICK_ACTION_CREATOR"
 
 const initialState = {
-  value:0,
+  baseParameters: {
+    strength: 0,
+    agility: 0,
+    intelligence: 0,
+    charisma: 0,
+  },
+  calculatedParameters:{
+    vitality: 0, // base = 3 + strength; action kick vitality -= 1;
+    dodge: 0, // base = 10 + agility
+    energy: 0 // base = agility + intelligence
+  },
+  skills:{
+    levelsNames:[
+      "untrained", "beginner", 
+      "apprentice", "adept", 
+      "expert", "master"],
+    strength:{
+      attack: 0
+    },
+    agility:{
+      stealth: 0,
+      archery: 0
+    },
+    intelligence:{
+      educability: 0,
+      survivability: 0,
+      healing: 0
+    },
+    charisma:{
+      intimidation: 0,
+      insight: 0,
+      appearance: 0,
+      manipulation: 0
+    }
+  },
+  heroes:[{
+    name: "Frodo",
+    baseParameters:{
+      strength: 2,
+      agility: 5,
+      intelligence: 3,
+      charisma: 4,
+    },
+    calculatedParameters:{
+      vitality: 5, // base = 3 + strength; action kick vitality -= 1;
+      dodge: 15, // base = 10 + agility
+      energy: 8 // base = agility + intelligence
+    },
+    skills:{
+      levels:["untrained", "beginner", 
+        "apprentice", "adept", 
+        "expert", "master"],
+      strength:{
+        attack: 2
+      },
+      agility:{
+        stealth: 5,
+        archery: 0
+      },
+      intelligence:{
+        educability:2,
+        survivability:1,
+        healing:0
+      },
+      charisma:{
+        intimidation: 0,
+        insight: 2,
+        appearance: 2,
+        manipulation: 0
+      }
+    },
+  },{}]
 };
 
 function heroesReducer(state = initialState, action) {
   switch (action.type) {
-  // case ACTION:
+  // case KICK_ACTION_CREATOR:
   //   return {
-  //     ...state,
+  //     ...state, calculatedParameters.vitality - 1
   //   };
   default: {
     return state;
