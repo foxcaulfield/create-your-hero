5191c44d4ccc0b47268ec212bdce0f3d97166afb
Fox Caulfield <mrcoderprog@gmail.com>
2021-04-20 12:10:03 +0300

    Add basename to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import store from "./store/reduxStore";
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <Route exact path="/"></Route>
         <Redirect to="/choose" />
